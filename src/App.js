dccb28f4f7ab399faddd3ebce7da88bb27acced4
Alfiya6282823310 <knalfiya@gmail.com>
2024-07-01 16:00:53 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import SignIn from './Components/SignIn';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signup from './components/Signup';
+import Signup from './Components/Signup';
 import Navbar from './components/Navbar';
 
 function App() {
