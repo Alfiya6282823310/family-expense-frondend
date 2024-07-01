709d2f01ae3699aadf312beef34542e3a4e7357e
AyshaTA <ayshata2019@gmail.com>
2024-07-01 10:07:51 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/signin' element={<SignIn/>}></Route></Routes></BrowserRouter>
+      <SignIn/>
   );
 }
 
